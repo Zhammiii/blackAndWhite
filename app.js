2020ad776ab184e7ba3b6aedf7421bd6209cd311
Samantha ossandon <ossandonsubiabre@gmail.com>
2024-04-03 14:43:02 -0300

    urls activas

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
-const Jimp = require('jimp')
-const fs = require('fs');
-const { v4 } = require('uuid');
-
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const Jimp = require("jimp");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 /* middlewares*/
-app.use('/css', express.static(__dirname + "/public/assets/css/"));
-
+app.use("/css", express.static(__dirname + "/public/assets/css/"));
 
 /* Rutas */
-app.get('/', (req , res) => {
-    try {
-        res.sendFile(`${__dirname}/public/index.html`);
-    } catch (error) {
-        res.status(500).send(error.message); 
+app.get("/", (req, res) => {
+  try {
+    res.sendFile(`${__dirname}/public/index.html`);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get("/process", async (req, res) => {
+  try {
+    let urlData = req.query.urlData; 
+    console.log(urlData);
+    if (!urlData) {
+      throw new Error("Missing URL data");
     }
-})
-
-app.get('/process', async (req , res) =>{
-    try {
-        
-
-
-        
-    } catch (error) {
-        res.status(500).send(error.message); 
-    }
-} )
+    res.setHeader("Content-Type", "image/png");
+    const imagen = await Jimp.read(urlData);
+    let nombreImagen = uuidv4().slice(0, 8) + "-nuevaImagen.png";
+    const rutaImagen = `public/assets/img/${nombreImagen}`;
+    await imagen.resize(350, Jimp.AUTO).greyscale().writeAsync(rutaImagen);
+    const imagenData = fs.readFileSync(rutaImagen);
+    res.send(imagenData);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 /* Levantar servidor */
-app.listen (PORT, () =>{
-console.log('Conectado al puerto ' + PORT);
-})
+app.listen(PORT, () => {
+  console.log("Conectado al puerto " + PORT);
+});
